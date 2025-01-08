d46d0d19f46fa0e33a9fc8347465fbeb8dfe4bea
pisey-sk <oupisey.it@gmail.com>
2025-01-08 17:43:33 +0700

    update game hub

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "f8e3607080424c63a12e70d9dc506617",
   },
 });
