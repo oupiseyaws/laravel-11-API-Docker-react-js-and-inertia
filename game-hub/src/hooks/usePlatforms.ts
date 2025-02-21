f1c7596ee717de3456927a9e1c62fa440f7d9aab
oupisey.it <oupisey.it@gmail.com>
2025-02-21 22:54:14 +0700

    refactoring simplifying time calculations

--- game-hub/src/hooks/usePlatforms.ts
+++ game-hub/src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient,{ FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 import Platforms from "../data/platforms";
+import ms from "ms";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
@@ -13,7 +14,7 @@ export interface Platform {
 const usePlatforms = () => useQuery({
   queryKey: ["platforms"],
   queryFn: apiClient.getAll,
-  staleTime: 24 * 60 * 60 * 1000, //24h
+  staleTime: ms("24h"),
     // cacheTime: 24 * 60 * 60 * 1000, //24h
   initialData: Platforms
 })
