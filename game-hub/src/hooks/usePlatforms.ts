cd20d4016294c6a7d9052c15481cbc6de546edf7
oupisey.it <oupisey.it@gmail.com>
2025-02-21 17:13:10 +0700

    refactoring -remove duplicate interface

--- game-hub/src/hooks/usePlatforms.ts
+++ game-hub/src/hooks/usePlatforms.ts
@@ -1,11 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient,{ FetchResponse } from "../services/api-client";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>  useQuery<FetchResponse<Platform>,Error>({
+  queryKey: ["platform"],
+  queryFn: () =>
+    apiClient.get<FetchResponse<Platform>>("/platforms/lists/parents").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: {count : platforms.length, results: platforms},
+})
 
 export default usePlatforms;
